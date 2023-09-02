a15d007bb980608af5437801ad26914334bc0561
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-09-02 17:05:06 -0300

    App Bar, exclusão de salas

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,55 +1,99 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Box, Button, Typography, Card, AppBar } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 
 function Rooms() {
-    const [rows, setRows] = useState([]);
-    const navigate = useNavigate();
+  const [rows, setRows] = useState([]);
+  const navigate = useNavigate();
 
+  const handleSala = (id) => {
+    navigate(`/sala/${id}`);
+  };
 
-    const handleSala = (id) => {
-        navigate(`/sala/${id}`)
-      };
-    
+  const fetchSalas = async () => {
+    try {
+      const response = await fetch('http://10.0.0.120/apiSala/salas/listar');
+      const responseData = await response.json();
 
-    const fetchSalas = async () => {
-        try {
-          const response = await fetch('http://10.0.0.120/apiSala/salas/listar');
-          const responseData = await response.json();
-    
-          if (responseData.tipo === 'sucesso') {
-            setRows(responseData.resposta);
-          } else {
-            console.log('Nenhum registro');
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
+      if (responseData.tipo === 'sucesso') {
+        setRows(responseData.resposta);
+      } else {
+        console.log('Nenhum registro');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-      useEffect(() => {
-        fetchSalas(); // Buscar usuários inicialmente
-    
-        const interval = setInterval(fetchSalas, 3000); // Atualizar a cada 5 segundos
-    
-        return () => {
-          clearInterval(interval); // Limpar intervalo ao desmontar o componente
-        };
-      }, []);
-    
+  useEffect(() => {
+    fetchSalas();
+
+    const interval = setInterval(fetchSalas, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div>
-      <h2>Salas existentes</h2>
-      {rows.map((row) => (
-     <div key={row.id}>
-        <p>Nome da Sala: {row.nome}</p>
-        <p>Data de  Criação: {row.created_at}</p>
-        <p>Criado por: {row.usuario_id}</p>
-        <button onClick={() => handleSala(row.id)}>Salas Existentes</button>
-        <p>-----------------------</p>
-     </div>
-       ))}
+      <AppBar sx={{ justifyContent: 'center', display: 'flex', flexDirection:'row', width:'100%', alignItems:'center',  height:'65px' }}>
+        <Box sx={{ width: '10%' }}>
+          <Button
+          variant="contained"
+            onClick={() => {
+              navigate(`/`);
+            }}
+            startIcon={<ArrowBackIcon />} // Use o ícone corretamente aqui
+            sx={{ marginBottom: '10px' }}
+          >
+            Voltar
+          </Button>
+        </Box>
+        <Box  sx={{ width: '75%' }}>
+          <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', marginBottom: '10px' }}>
+            Salas Online
+          </Typography>
+        </Box>
+      </AppBar>
+
+      <Box
+        sx={{
+          width: '100',
+          height: '100vh',
+          justifyContent: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            width: '70%',
+            height: '60vh',
+            justifyContent: 'center',
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '16px',
+          }}
+        >
+          {rows.map((row) => (
+            <Card key={row.id} sx={{ padding: '25px', width: '250px', height: '200px' }}>
+              <p>Nome da Sala: {row.nome}</p>
+              <p>Data de Criação: {row.created_at}</p>
+              <p>Criado por: {row.usuario_id}</p>
+              <Button onClick={() => handleSala(row.id)}>Entrar</Button>
+            </Card>
+          ))}
+        </Box>
+      </Box>
     </div>
   );
 }
--- src/components/Sala.js
+++ src/components/Sala.js
@@ -1,14 +1,153 @@
 import React, { useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom'; // Importe o hook useParams
+import { Box, Button, Typography, AppBar, Menu, MenuItem  } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import AppsIcon from '@mui/icons-material/Apps';
+import { useNavigate } from 'react-router-dom';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function Sala() {
-   
-    const { id } = useParams();
+  const navigate = useNavigate();
+  const [row, setRows] = useState('');
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const { id } = useParams();
 
+    const handleClickMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleCloseMenu = () => {
+      setAnchorEl(null);
+    };
+  const handleClickOpenDialogDelete =  () => {
+    setOpenDialog(true);
+    handleCloseMenu()
+
+  };
+  const handleCloseDialogDelete = () => {
+    setOpenDialog(false);
+  };
+
+
+  const handleClickDelete =  async () => {
+   
+    try {
+      const response = await fetch(`http://10.0.0.120/apiSala/salas/deletar/${id}`, {
+      method: 'DELETE', // Use o método DELETE
+    });
+      const responseData = await response.json();
+
+      if (responseData.tipo === 'sucesso') {
+     
+        console.log('Sala Excluida')
+         navigate('/lobby'); 
+      } else {
+        console.log('Nenhum registro');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+ 
+
+    const fetchSalas = async () => {
+      try {
+        const response = await fetch(`http://10.0.0.120/apiSala/salas/listar/${id}`);
+        const responseData = await response.json();
+  
+        if (responseData.tipo === 'sucesso') {
+          setRows(responseData.resposta);
+        } else {
+          console.log('Nenhum registro');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchSalas();
+  
+      const interval = setInterval(fetchSalas, 3000);
+  
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
 
   return (
     <div>
-      <h2>Salas: {id}</h2>
+     <AppBar sx={{ justifyContent: 'center', display: 'flex', flexDirection:'row', width:'100%', alignItems:'center',  height:'65px' }}>
+        <Box sx={{ width: '10%' }}>
+          <Button
+          variant="contained"
+            onClick={() => {
+              navigate(`/lobby`);
+            }}
+            startIcon={<ArrowBackIcon />} 
+            sx={{ marginBottom: '10px' }}
+          >
+            Voltar
+          </Button>
+        </Box>
+        <Box  sx={{ width: '75%' }}>
+          <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', marginBottom: '10px' }}>
+            Sala {row.nome}
+          </Typography>
+        </Box>
+
+        <Button
+        variant="contained"
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClickMenu}
+      >
+        <AppsIcon/>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleCloseMenu}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleCloseMenu}>Editar Nome</MenuItem>
+        <MenuItem onClick={handleClickOpenDialogDelete}>Excluir Sala</MenuItem>
+        <MenuItem onClick={handleCloseMenu}>Modo Offline</MenuItem>
+      </Menu>
+
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseDialogDelete}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Exclusão de Sala"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Voce está preste a excluir a sala virtual "{row.nome}". Tem certeza dessa descisão?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialogDelete}>Não</Button>
+          <Button onClick={handleClickDelete} autoFocus>Sim</Button>
+        </DialogActions>
+      </Dialog>
+
+      </AppBar>
+
+       
       
     </div>
   );
